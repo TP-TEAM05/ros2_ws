cc55a5f19903e31a2ec738e00c14c68481da59b2
Andrej Horváth <114003194+AndrejHstu@users.noreply.github.com>
2025-10-26 22:23:12 +0100

    scan_subscriber proto

--- new_ros/tp5_ros/src/my_node.cpp
+++ new_ros/tp5_ros/src/my_node.cpp
@@ -1,10 +1,29 @@
 #include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+using std::placeholders::_1;
+
+class MinimalSubscriber: public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber"){
+      subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "scan", 10, std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
+    }
+    private:
+    void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->angle_min.c_str());
+    }
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+};
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world tp5_ros package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
   return 0;
 }
